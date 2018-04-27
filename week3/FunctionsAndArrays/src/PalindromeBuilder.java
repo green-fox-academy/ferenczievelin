8da0f7abecc10f928d528a68c73b4b0c1938028d
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-04-27 19:40:05 +0200

    Palindromebuilder

--- week3/FunctionsAndArrays/src/PalindromeBuilder.java
+++ week3/FunctionsAndArrays/src/PalindromeBuilder.java
@@ -5,9 +5,15 @@ public class PalindromeBuilder {
         System.out.println("Enter a word: ");
         Scanner scan = new Scanner(System.in);
         String wordpali= scan.nextLine();
+        System.out.println(palindromeBuilder(wordpali));
 
     }
     public static String palindromeBuilder (String word) {
+        StringBuilder str = new StringBuilder();
 
+        for(int i=word.length()-1; i>0; i--){
+            str.append(word.charAt(i-1));}
+        String pala =word+ str.toString();
+        return pala;
     }
 }
