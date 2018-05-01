bb534d44c3093a2013f24b5cfebb901f4ab69702
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-05-01 12:30:34 +0200

    calculator

---
+++ week2/Friyay/DataStructures/src/Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Enter the expression: ");
+        Scanner scanner = new Scanner(System.in);
+        String op = scanner.next();
+        System.out.println("Enter the first number");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the second number");
+        int secondNumber = scanner.nextInt();
+        System.out.println(calculator(op, firstNumber, secondNumber));
+
+
+    }
+    public static double calculator (String operation, int op1, int op2){
+        double answer=0;
+        switch (operation){
+            case "+" :
+                answer = op1 + op2;
+                break;
+            case "-" :
+                answer = op1 - op2;
+                break;
+            case "*":
+                answer = op1 * op2;
+                break;
+            case "/":
+                answer = op1 / op2;
+                break;
+        }
+        return answer;
+        }
+    }
+
+
