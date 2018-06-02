ce06ce3bc61b3b24c6b675c67076656fc8d49573
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-06-02 09:58:24 +0200

    springboot

---
+++ week9/SpringBoot/src/main/java/hu/greenfoxacademy/springstart/Controllerz/Controller.java
@@ -0,0 +1,23 @@
+package hu.greenfoxacademy.springstart.Controllerz;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@RestController
+public class Controller {
+    private String name;
+    @GetMapping("/cicak")
+    public ArrayList<String> getCicaNames () {
+        return new ArrayList<>(Arrays.asList(
+                "cirmi",
+                "garfield"
+        ));
+    }
+
+
+
+}
---
+++ week9/SpringBoot/src/main/java/hu/greenfoxacademy/springstart/Controllerz/Greeting.java
@@ -0,0 +1,7 @@
+package hu.greenfoxacademy.springstart.Controllerz;
+
+public class Greeting {
+    private long id;
+    private String content;
+
+}
