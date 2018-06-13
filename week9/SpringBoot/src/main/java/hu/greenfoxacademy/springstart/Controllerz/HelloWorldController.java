13aacc99f46c4f61ffabda2deb6f0d18df0a9253
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-06-13 20:54:54 +0200

    spring counter-half-ready

--- week9/SpringBoot/src/main/java/hu/greenfoxacademy/springstart/Controllerz/HelloWorldController.java
+++ week9/SpringBoot/src/main/java/hu/greenfoxacademy/springstart/Controllerz/HelloWorldController.java
@@ -5,12 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloWorldController {
+  //  private static final String hello = "Hello, %s!";
+    AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/web/greeting")
-    public String greeting(Model model @RequestParam) {
-        model.addAttribute("name", " World");
+    public String greeting(Model model, @RequestParam("name") String name) {
+        model.addAttribute("name", name);
+        model.addAttribute("id", counter.incrementAndGet());
         return "greeting";
     }
 }
