0d21985000fe2ffa1fe4dd339510d2ab374f1e49
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-06-20 18:28:54 +0200

    connect to sql not ready

---
+++ week11/ConnectMySQL/src/main/java/com/greenfoxacademy/notillegalcharacters/NotillegalcharactersApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.notillegalcharacters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotillegalcharactersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotillegalcharactersApplication.class, args);
+    }
+}
---
+++ week11/ConnectMySQL/src/test/java/com/greenfoxacademy/notillegalcharacters/NotillegalcharactersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.notillegalcharacters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotillegalcharactersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
