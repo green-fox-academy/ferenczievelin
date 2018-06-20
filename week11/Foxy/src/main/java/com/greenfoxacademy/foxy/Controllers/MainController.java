e74c2c1a69dbd704402ef36498a439824ea00a47
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-06-20 20:32:33 +0200

    login page

--- week11/Foxy/src/main/java/com/greenfoxacademy/foxy/Controllers/MainController.java
+++ week11/Foxy/src/main/java/com/greenfoxacademy/foxy/Controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.foxy.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,6 +9,10 @@ public class MainController {
     @RequestMapping(value = "/")
     public String information() {
         return "index";};
+
+    @GetMapping(value = "/login")
+    public String name() {
+        return "login";};
 
 }
 
