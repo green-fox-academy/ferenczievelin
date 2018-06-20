8ee307ae461353d073af87c7e7f2af53321d25f1
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-06-20 19:59:25 +0200

    intro

---
+++ week11/Foxy/src/main/java/com/greenfoxacademy/foxy/FoxyApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.foxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxyApplication.class, args);
+    }
+}
---
+++ week11/Foxy/src/main/java/com/greenfoxacademy/foxy/Model/Model.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.foxy.Model;
+
+public class Model {
+}
