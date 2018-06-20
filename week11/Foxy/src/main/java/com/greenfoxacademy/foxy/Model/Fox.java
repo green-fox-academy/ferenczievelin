57f87074964649c239b65d20cca5b8d4b8ffe73e
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-06-20 20:55:42 +0200

    created fox class with the main attributes

--- week11/Foxy/src/main/java/com/greenfoxacademy/foxy/Model/Fox.java
+++ week11/Foxy/src/main/java/com/greenfoxacademy/foxy/Model/Fox.java
@@ -1,5 +1,17 @@
 package com.greenfoxacademy.foxy.Model;
 
+import java.util.List;
+
 public class Fox {
     String name;
+    List<String> tricks;
+
+    enum food {
+        pizza, salad, nachos, rakottkrumpli, toltottkaposzta
+    }
+
+    enum drink {
+        wine, vodka, rum, pinaColada
+    }
+
 }
