2a34bc1da07ee66e831b228ab2e9989d274a8e9f
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-06-20 20:45:59 +0200

    login page without optional

--- week11/Foxy/src/main/java/com/greenfoxacademy/foxy/Controllers/MainController.java
+++ week11/Foxy/src/main/java/com/greenfoxacademy/foxy/Controllers/MainController.java
@@ -2,17 +2,29 @@ package com.greenfoxacademy.foxy.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @RequestMapping(value = "/")
     public String information() {
-        return "index";};
+        return "index";
+    }
+
+    ;
 
     @GetMapping(value = "/login")
     public String name() {
-        return "login";};
+        return "login";
+    }
 
+    ;
+
+    @PostMapping
+    public String login(@RequestParam(value = "name") String name) {
+        return "redirect:/?name=" + name;
+    }
 }
 
