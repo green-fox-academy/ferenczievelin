9a5033154c6dd88ca090f70368dd7bf09851e670
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-07-04 19:55:56 +0200

    add: adding post

--- week14/src/main/java/com/grenfoxacademy/reddit/Controller/PostController.java
+++ week14/src/main/java/com/grenfoxacademy/reddit/Controller/PostController.java
@@ -1,10 +1,11 @@
 package com.grenfoxacademy.reddit.Controller;
 
 import com.grenfoxacademy.reddit.Model.Post;
-import com.grenfoxacademy.reddit.Service.PostService;
 import com.grenfoxacademy.reddit.Service.PostServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +22,12 @@ public class PostController {
     @GetMapping("posts")
     public List<Post> getAllPosts () {
         return postService.getAllPosts();
+    }
+
+    @PostMapping("posts")
+    public List<Post> addPost (@RequestBody Post post) {
+        postService.addPost(post);
+        return (List<Post>) postService.getAllPosts();
     }
 
 }
