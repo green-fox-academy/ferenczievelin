b44e5c0123206369274b6db34fc8299255e195c9
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-07-09 18:28:07 +0200

    no user, no error

--- week14/src/main/java/com/grenfoxacademy/reddit/Repository/VoteRepo.java
+++
@@ -1,8 +0,0 @@
-package com.grenfoxacademy.reddit.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoteRepo extends CrudRepository {
-}
