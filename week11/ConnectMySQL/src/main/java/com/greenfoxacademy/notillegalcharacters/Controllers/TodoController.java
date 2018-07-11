370c3d7f1143e9f59ab2f0b6f9c99cbcdde1b0e0
ferenczievelin <37547193+ferenczievelin@users.noreply.github.com>
2018-07-11 19:22:09 +0200

    base of todo with database

--- week11/ConnectMySQL/src/main/java/com/greenfoxacademy/notillegalcharacters/Controllers/TodoController.java
+++ week11/ConnectMySQL/src/main/java/com/greenfoxacademy/notillegalcharacters/Controllers/TodoController.java
@@ -1,4 +1,16 @@
 package com.greenfoxacademy.notillegalcharacters.Controllers;
 
+import com.greenfoxacademy.notillegalcharacters.Service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TodoController {
+
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
 }
---
+++ week11/ConnectMySQL/src/main/java/com/greenfoxacademy/notillegalcharacters/Service/ServiceImpl.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.notillegalcharacters.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceImpl implements TodoService {
+}
